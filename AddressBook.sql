e94fa057ba520ce993633f0c0befea0f7de28e01
Thabiso <62402540+ThabisoHlatshwayo@users.noreply.github.com>
2020-07-09 11:10:13 +0200

    Add files via upload

---
+++ AddressBook.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `addresses` (
+	`AddressID` INT(50) NOT NULL auto_increment,			
+	`FirstName` VARCHAR(50),
+	`Lastname` VARCHAR(50),
+	`Email` VARCHAR(50),
+	`PhoneNumber` VARCHAR(50),
+	PRIMARY KEY (`AddressID`)
+);
+ 
---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main  {
+	// main method
+	public static void main( String args[] ){
+		AddressBookDisplay addressbook = new AddressBookDisplay();
+	} // end method main
+} // end class Main
